026d30ea09236972d04d5bccdd90917557cf6011
danielorrala555-spce <danielorrala5552@gmail.com>
2026-06-27 13:23:34 -0500

    agregar funsiones de movimiento

--- limones.js
+++ limones.js
@@ -5,7 +5,7 @@ ctx=canvas.getContext("2d");
 const ALTURA_SUELO =30;
 const ALTURA_PERSONAJE =40;
 const ANCHO_PERSONAJE =80;
-
+let personajex = canvas.width/2-40
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -17,5 +17,26 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="#F5A327";
-    ctx.fillRect(canvas.width/2-40,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajex,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajex=personajex-10;
+    refrescarPantalla();
+    dibujarPersonaje();
+    dibujarSuelo();
+
+}
+
+function moverDerecha(){
+    personajex=personajex+10;
+    refrescarPantalla();
+    dibujarPersonaje();
+    dibujarSuelo();
+
+}
+
+function refrescarPantalla(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    
 }
