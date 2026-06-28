dd4fd6afef19ff8b493d041311421ac096d5e696
daniel-000com <danielorrala5552@gmail.com>
2026-06-27 22:16:24 -0500

    add bajarLimon & actualizarPantalla functions

--- limones.js
+++ limones.js
@@ -5,10 +5,17 @@ ctx=canvas.getContext("2d");
 const ALTURA_SUELO =30;
 const ALTURA_PERSONAJE =40;
 const ANCHO_PERSONAJE =80;
+const ALTURA_LIMON =40;
+const ANCHO_LIMON =40;
 let personajex = canvas.width/2-40
+let limonX = canvas.width/2-40;
+let limonY = 0;
+
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function dibujarSuelo(){
@@ -22,21 +29,34 @@ function dibujarPersonaje(){
 
 function moverIzquierda(){
     personajex=personajex-10;
-    refrescarPantalla();
-    dibujarPersonaje();
-    dibujarSuelo();
-
+     actualizarPntalla();
+   
 }
 
 function moverDerecha(){
     personajex=personajex+10;
-    refrescarPantalla();
-    dibujarPersonaje();
-    dibujarSuelo();
-
+    actualizarPntalla();
+   
 }
 
 function refrescarPantalla(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+   
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="#61f527";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+}
+function bajarLimon(){
+    limonY = limonY +10;
+    actualizarPntalla();
     
+}
+
+function actualizarPntalla(){
+    refrescarPantalla();
+    dibujarPersonaje();
+    dibujarSuelo();
+    dibujarLimon();
 }
