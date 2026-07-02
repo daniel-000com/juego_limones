7d060017143394aaec5fc70995d33cd0e117eacf
daniel-000com <danielorrala5552@gmail.com>
2026-07-01 23:21:46 -0500

    add aleatorio funtion

--- limones.js
+++ limones.js
@@ -2,18 +2,19 @@ let canvas;
 let ctx;
 canvas=document.getElementById("areaJuegos");
 ctx=canvas.getContext("2d");
-let ALTURA_SUELO =30;
-let ALTURA_PERSONAJE =10;
-let ANCHO_PERSONAJE =20;
-const ALTURA_LIMON =80;
-const ANCHO_LIMON =80;
-let personajex = canvas.width/2-40
+const ALTURA_SUELO =30;
+const ALTURA_PERSONAJE =40;
+const ANCHO_PERSONAJE =80;
+const ALTURA_LIMON =40;
+const ANCHO_LIMON =40;
+let personajex = canvas.width/2-ANCHO_PERSONAJE/2;
 let personajeY = canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX = canvas.width/2-40;
 let limonY = 0;
 let puntaje=0;
 let vidas=5;
 let velocidaCaida=200;
+let aleatorio=3;
 
 
 
@@ -90,8 +91,16 @@ function detectarColicion(){
         }
 }
 function probarAleatorio(){
-    let aleatorio=generaAleatorio(10,80);
-    console.log (aleatorio);
+    personajex=generaAleatorio(0,canvas.width-ANCHO_PERSONAJE);
+    actualizarPantalla();
+    aleatorio=aleatorio-1;
+        mostrarSpam("contadorAleatorio",aleatorio);
+    if (aleatorio==0){
+        alert("ya no puedes usar mas el boton");
+        let componente=document.getElementById("btnAleatorio");
+        componente.disabled=true;
+    }
+     
 }
 function aparecerLimon(){
     limonX=generaAleatorio(0,canvas.width-ANCHO_LIMON);
@@ -115,7 +124,11 @@ function detectarPiso(){
 function reiniciar(){
     puntaje = 0;
     vidas = 5;
+    aleatorio=3;
+    let componente=document.getElementById("btnAleatorio");
+    componente.disabled=false;
     personajex =canvas.width / 2-40;
+    document.getElementById("contadorAleatorio").textContent = aleatorio;
     document.getElementById("txtPuntaje").textContent = puntaje;
     document.getElementById("txtVidas").textContent = vidas;
     clearInterval(interval);
@@ -126,4 +139,5 @@ function reiniciar(){
 
 function desaparecerPersonaje(){
     ctx.clearRect(personajex, personajeY, ANCHO_PERSONAJE, ALTURA_PERSONAJE);
+
 }
